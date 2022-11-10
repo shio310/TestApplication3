b1bea8c3518a93b75b5999766ed77ae0434c501d
TakuShiozawa <taku_0310@hotmail.com>
2022-11-10 19:07:35 +0900

    プロジェクト ファイルを追加します。

---
+++ TestApplication3/Class1.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// サンプルクラスライブラリ
+/// </summary>
+namespace ClassLibrary
+{
+    /// <summary>
+    /// サンプルクラス
+    /// </summary>
+    public class Class1
+    {
+
+        /// <summary>
+        /// サンプルプロパティ
+        /// </summary>
+        public int someNumber = 1;
+
+        /// <summary>
+        /// サンプルメソッド
+        /// </summary>
+        /// <param name="text">サンプル引数</param>
+        public void DoSomething(string text)
+        {
+            System.Diagnostics.Debug.WriteLine(text);
+        }
+
+    }
+}
---
+++ TestApplication3/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary;
+
+Console.WriteLine("Hello, World!");
+
+var t = new Class1();
